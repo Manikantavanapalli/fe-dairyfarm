668c0c47f693225d783bbb4a4eaf5ba2829eaf58
vanapalli manikanta <manikantavanapalli136@gmail.com>
2025-02-06 22:00:57 +0530

    added bootstrap lib

--- dairy-farm/src/pages/HomePage/index.tsx
+++ dairy-farm/src/pages/HomePage/index.tsx
@@ -1,6 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const Home: React.FC = () => {
-    return <h1>Welcome to the Dairy Farm!</h1>;
-  };
-  
-  export default Home;
-  
+  return (
+    <div className="container text-center py-5 bg-light">
+      <h1 className="text-primary display-3">Welcome to the Dairy Farm!</h1>
+    </div>
+  );
+};
+
+export default Home;
