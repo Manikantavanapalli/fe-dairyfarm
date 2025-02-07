882510cd51ac62b04238b596169991e4a8342cc6
vanapalli manikanta <manikantavanapalli136@gmail.com>
2025-02-08 01:05:43 +0530

    added code

--- dairy-farm/src/components/Header/index.tsx
+++ dairy-farm/src/components/Header/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../assets/images/LV Logo.png";
 
 const Header: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <Link className="navbar-brand" to="/">Milk Dairy</Link>
+        <Link className="navbar-brand" to="/">
+          <img
+            src={logo}
+            alt="Milk Dairy Logo"
+            style={{ height: "60px",width:"60px" }}
+          />
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -22,6 +29,9 @@ const Header: React.FC = () => {
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/login">Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">Register</Link>
             </li>
           </ul>
         </div>
